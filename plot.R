6420691b5e9c0ede3f49f6f37d13abf13c09b56e
Andrew Zimolzak (Dell Core i5) <andyzimolzak@gmail.com>
2017-10-24 13:59:10 -0400

    Make better looking bars like I wanted.

--- plot.R
+++ plot.R
@@ -1,7 +1,8 @@
 library(ggplot2)
+# packageVersion('ggplot2')
 X = read.csv("C:/Users/vhabhszimola/Documents/local/solitaire_viz/intermediate.csv", header=FALSE, stringsAsFactors=FALSE)
 
-v = (X[1,])
+v = (X[18,])
 j1_idx = 0
 j2_idx = 0
 
@@ -20,4 +21,4 @@ for(i in c(1:length(v))){
 }
 
 ts = data.frame(t(v), pos = c(1:54))
-ggplot(ts, aes(pos, X1)) + geom_point() + geom_vline(xintercept = j1_idx) + geom_vline(xintercept = j2_idx)
+ggplot(ts, aes(pos, X18)) + geom_bar(stat='identity', width=1) + geom_vline(xintercept = j1_idx) + geom_vline(xintercept = j2_idx)
